c8e781dce1846cda22be2d4a6003944fb58fee8d
Yadnesh <yadneshdongre24@gmail.com>
2026-07-29 11:15:05 +0530

    My first commit

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Spread Love!")
         }
         .padding()
     }
