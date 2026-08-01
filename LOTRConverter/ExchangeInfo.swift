ad58fea00efbf3e875682d78e91810eb247eb34c
Yadnesh <yadneshdongre24@gmail.com>
2026-08-01 12:39:13 +0530

    [Feature] add new SwiftUI view, reorganize stacks, modify existing views, and test dark mode support

---
+++ LOTRConverter/ExchangeInfo.swift
@@ -0,0 +1,65 @@
+//
+//  ExchangeInfo.swift
+//  LOTRConverter
+//
+//  Created by Yadnesh Dongre on 01/08/26.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        ZStack {
+            // Background parchment image
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                // Title text
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(2)
+                
+                // Description text
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(.title3)
+                    .padding()
+                
+                // Exchange rates
+                HStack {
+                    // Left currency image
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                    
+                    // Exchange rate text
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    
+                    // Right currency image
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
+                }
+                
+                // Done Button
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .foregroundStyle(.black)
+        }
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
